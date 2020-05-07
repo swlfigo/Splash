d734d48317c1fd22e30f34ce61edc4cd4e5d752e
Sylar <swlfigo@gmail.com>
2020-05-07 23:13:33 +0800

    first commit

---
+++ Splash/Module/Base/SplashMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashMainViewController.swift
+//  Splash
+//
+//  Created by Sylar on 2020/5/7.
+//  Copyright © 2020 Sylar. All rights reserved.
+//
+
+import UIKit
+
+class SplashMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Splash/Module/Base/SplashNavigatonController.swift
@@ -0,0 +1,21 @@
+//
+//  SplashNavigatonController.swift
+//  Splash
+//
+//  Created by Sylar on 2020/5/7.
+//  Copyright © 2020 Sylar. All rights reserved.
+//
+
+import UIKit
+
+class SplashNavigatonController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationBar.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
